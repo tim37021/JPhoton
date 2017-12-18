60d6a3abc9908832431ee77882bbdf1179e98233
amikai <as23041248@gmail.com>
2017-12-18 16:19:23 +0800

    Add constructor for Intersection class

--- src/core/Intersection.java
+++ src/core/Intersection.java
@@ -3,4 +3,10 @@ package core;
 public final class Intersection {
 	public Vector3f point;
 	public Vector3f normal;
+	
+	public Intersection(Vector3f point, Vector3f normal) {
+		this.point = point;
+		this.normal = normal;
+	}
+
 }
