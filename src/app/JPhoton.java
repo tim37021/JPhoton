a84b7638c718f168fb81e41fd5a80051ebd2b484
tim37021 <tim37021@gmail.com>
2017-12-18 17:42:52 +0800

    Our first test

--- src/app/JPhoton.java
+++ src/app/JPhoton.java
@@ -2,21 +2,28 @@ package app;
 
 import camera.*;
 import core.*;
+import primitive.*;
 
-public class JPhoton{
+public class JPhoton {
+	static private Triangle tri;
 	static private Vector3f radiance(Ray r) {
-		return new Vector3f(1, 1, 1);
+		if(tri.intersect(r) != null)
+			return new Vector3f(1, 1, 1);
+		else
+			return new Vector3f(0, 0, 0);
 	}
 	
     static public void main(String args[]) {
     	Canvas canvas = new Canvas(800, 600);
     	Camera cam = new PerspectiveCamera(45.0f*3.1415f/180.0f, 0.1f, new Vector2f(800, 600));
+    	tri = new Triangle(new Vector3f(-2, -2, 10), new Vector3f(2, -2, 10), new Vector3f(0,  2, 10));
+    
     	for(int y=0; y<600; y++) {
     		for(int x=0; x<800; x++) {
     			Ray r = cam.shootRay(x, y);
     			canvas.setPixel(x, y, radiance(r));
     		}
     	}
-    	canvas.saveToFile("GGVERYIN.png");
+    	canvas.saveToFile("outImg/GGVERYIN.png");
     }
 }
